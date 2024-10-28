d955681e25952cab8c0ee635eab97890340e38c2
rvjq <rvjq@cesar.school>
2024-10-28 20:30:31 -0300

    Added Snake

--- src/main.c
+++ src/main.c
@@ -15,7 +15,13 @@ typedef struct snakePart {
 
 void drawBorders (screenColor bg);
 
-void drawSnake();
+void drawSnake(SNAKEPART *snakeHead);
+
+void addSnake(SNAKEPART *snakeHead, int x, int y);
+
+SNAKEPART *spawnSnake();
+
+void despawnSnake(SNAKEPART *snakeHead);
 
 int main() {
     screenInit(0);
@@ -23,21 +29,63 @@ int main() {
     timerInit(100);
 
     drawBorders(DARKGRAY);
+    SNAKEPART *head = spawnSnake();
 
+    drawSnake(head);
+    
     while (1) {
         if(keyhit()) {
             if (readch() == 27) break;
         }
         screenUpdate();
     }
+
+    despawnSnake(head);
     timerDestroy();
     keyboardDestroy();
     screenDestroy();
     return 0;
 }
 
-void drawSnake () {
+SNAKEPART *spawnSnake() {
+    SNAKEPART *head = malloc(sizeof(SNAKEPART));
+    head->X = (MAXX/2)+1;
+    head->Y = (MAXY/2);
+    head->prev = NULL;
+    head->next = NULL;
+    addSnake(head, MAXX/2, MAXY/2);
+    addSnake(head, (MAXX/2)-1, (MAXY/2));
+    return head;
+}
+
+void despawnSnake(SNAKEPART *snakeHead) {
+    free(snakeHead);
+}
+
+void addSnake(SNAKEPART *snakeHead, int x, int y) {
+    SNAKEPART *temp = snakeHead->next;
+    while(temp != NULL) {
+        snakeHead = temp;
+        temp = temp->next;
+    }
+    snakeHead->next = malloc(sizeof(SNAKEPART));
+    temp = snakeHead->next;
+    temp->X = x;
+    temp->Y = y;
+    temp->prev = snakeHead;
+    temp->next = NULL;
+}
+
+void drawSnake(SNAKEPART *snakeHead) {
     // ╭╮╰╯│─ ▲►▼◄
+    screenGotoxy(snakeHead->X, snakeHead->Y);
+    printf("►");
+    SNAKEPART *temp = snakeHead->next;
+    while(temp != NULL) {
+        screenGotoxy(temp->X,temp->Y);
+        printf("─");
+        temp = temp->next;
+    }
 }
 
 void drawBorders (screenColor bg) {
